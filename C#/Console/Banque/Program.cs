5c55ea506a255d6d6b47a95e460f7172d1dd1f2c
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-03-07 14:38:24 +0100

    Création de la classe Compte

---
+++ C#/Console/Banque/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Banque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account newAccount = new Account("Nafrere", "Christopher", "26/07/2000", "11 rue de nezel");
+            newAccount.reception(1500);
+
+            Console.WriteLine("\n");
+
+            newAccount.getReleverBancaire();
+
+        }
+    }
+}
