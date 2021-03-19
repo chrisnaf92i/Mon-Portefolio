4f94a16e2a2ee9eeb9abc2a4462ced11e6675a2f
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-03-19 19:24:59 +0100

    Ajout graphique et changement téchnique

--- Exercice Graven/Jour 7 Generateur de citation/script.js
+++ Exercice Graven/Jour 7 Generateur de citation/script.js
@@ -4,6 +4,7 @@ let auther = document.getElementById("name");
 let img = document.getElementById("image");
 let citation = document.getElementById("citation");
 let listCitation = [];
+let linkPage = document.getElementById("auther-page");
 generateBtn.addEventListener("click", GenerateCitation);
 
 fetch(url).then((data) => 
@@ -22,6 +23,9 @@ function GenerateCitation()
 {
     let random = Math.floor(Math.random() * (listCitation.length-0));
     let randomCitation = listCitation[random];
+
+    linkPage.setAttribute("href", randomCitation["personnalPage"]);
+    
     auther.textContent =  randomCitation["nom"];
     img.setAttribute("src", randomCitation["image"]);
     citation.textContent = randomCitation["citation"];
